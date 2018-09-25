75116553a06f1bc48ea411e97dd8f57e416c8251
ArjunKulkarni <arjunkneworld@gmail.com>
2018-09-25 16:45:04 +0530

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,67 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Dashboard | Job Portal</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="jobs" />
+    <script type="application/x-javascript"> 
+		addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } 
+    </script>
+    
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script> -->
+    <script src="js/jquery-1.11.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<script src="editor.js"></script>
+    <!-- Custom Theme files -->
+    <link href="css/style.css" rel='stylesheet' type='text/css' />
+    <link href="css/editor.css" rel='stylesheet' type='text/css' />
+    <link href='css/fontFamily.css' rel='stylesheet' type='text/css'>
+    <!----font-Awesome----->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!----font-Awesome----->
+
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+<div class="container">
+	<?php include('loginwindow.php'); ?>
+	<?php include('loginmenu.php'); ?>
+</div>
+<div class="container">
+	<?php include('topMenu.php'); ?>
+</div>
+</nav>
+<br/>
+<?php
+if (isset($_SESSION["username"]))
+{
+	include('candidate/views/cadidateDashboardMain.php');
+//	include('candidateDashboard.php');
+}
+else
+if (isset($_SESSION["empusername"])){
+	include('employerDashboard.php');
+}
+else{
+
+}
+?>        
+<?php include('footer.php'); ?>
+<script>
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+});
+</script>
+
+</body>
+</html>
