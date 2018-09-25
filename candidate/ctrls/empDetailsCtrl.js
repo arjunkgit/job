82e396cf9109c92dd2fc5b4c883dda549d48d22d
ArjunKulkarni <arjunkneworld@gmail.com>
2018-09-25 17:49:03 +0530

    Create empDetailsCtrl.js

---
+++ candidate/ctrls/empDetailsCtrl.js
@@ -0,0 +1,212 @@
+candidateApp.controller("empDetailsCtrl", function ($scope, $rootScope, postData, $filter, $http, NgTableParams) {
+
+    var empDetailsData = {};
+    var stringWorkedJoinDate = "";
+    var stringWorkedEndDate = "";
+    var stringDob = "";
+    $rootScope.showLoader = true;
+    $scope.showAddEmpDetails = false;
+    $scope.hideChoosePhoto = true;
+    $scope.isDisable = false;
+    //empty dataset
+    $scope.empSelectedDetailsData = {};
+    // $scope.empSelectedDetailsData = [{
+    //     "id": "48",
+    //     "email": "user1@gmail.com",
+    //     "empHistory": "2010 - IT Administrator - Tosox Corp Oversaw the Companys IT infrastrucre, managed and maintaned the IT and multimedia network.",
+    //     "workedCompany": "cname22",
+    //     "workedYear": "",
+    //     "workedMonth": "",
+    //     "workedJoinDate": new Date("2015-03-25"),
+    //     "workedEndDate": new Date("2015-03-25"),
+    //     "workedCurrentJob": true
+    // },
+    // {
+    //     "id": "49",
+    //     "email": "user1@gmail.com",
+    //     "empHistory": "2010 - IT Administrator - Tosox Corp Oversaw the Companys IT infrastrucre, managed and maintaned the IT and multimedia network.",
+    //     "workedCompany": "cname22",
+    //     "workedYear": "",
+    //     "workedMonth": "",
+    //     "workedJoinDate": new Date("2015-03-25"),
+    //     "workedEndDate": new Date("2015-03-25"),
+    //     "workedCurrentJob": false
+    // }];
+
+    //get emp details
+    $scope.getEmpDetails = function () {
+        var getEmpDetailsData = { "tableName": "emphistory", "type": "getUserData" };
+        var cat2 = postData.getUserData(getEmpDetailsData);
+        cat2.then(function (response) {
+            console.log(response);
+            $scope.empSelectedDetailsData = response.data;
+            $scope.isDisable = true;
+            $rootScope.showLoader = false;
+        });
+    };
+
+    $scope.addEmpDetails = function () {
+        $("#addEmpDetailsPopup").modal({show: true, backdrop: "static", keyboard: false});
+        $scope.empShowDetailsData = {
+            workedYear : "",
+            workedMonth : "",
+            workedJoinDate : "",
+            workedEndDate : "",
+            workedCurrentJob : ""
+        };
+        $rootScope.showLoader = true;
+        $scope.isDisable = false;
+        $scope.showAddEmpDetails = true;
+        $rootScope.cancelBtn = true;
+        $rootScope.addBtn = false;
+        $rootScope.showLoader = false;
+    };
+
+  $scope.convertToDate = function (stringDate){
+    var dateOut = new Date(stringDate);
+    dateOut.setDate(dateOut.getDate());
+    return dateOut;
+  };
+    //onclick of update
+    $scope.updateIndEmpDetails = function (id, data){
+        $("#addEmpDetailsPopup").modal({show: true, backdrop: "static", keyboard: false});
+        $scope.empShowDetailsData = data;     
+        $rootScope.showLoader = true;
+        $scope.isDisable = false;
+        $scope.showAddEmpDetails = true;
+        $rootScope.cancelBtn = true;
+        $rootScope.addBtn = false;
+        $rootScope.showLoader = false;
+        $scope.empShowDetailsData.workedJoinDate = new Date($scope.empShowDetailsData.workedJoinDate);
+        $scope.empShowDetailsData.workedEndDate = new Date($scope.empShowDetailsData.workedEndDate);
+        console.log($scope.empShowDetailsData.workedCurrentJob );
+        if($scope.empShowDetailsData.workedCurrentJob ==  '') {
+            $scope.empShowDetailsData.workedCurrentJob = false;            
+        }else if($scope.empShowDetailsData.workedCurrentJob == "true"){
+            $scope.empShowDetailsData.workedCurrentJob = true;
+            $scope.empShowDetailsData.workedEndDate = "";
+        }else if($scope.empShowDetailsData.workedCurrentJob == "false"){
+            $scope.empShowDetailsData.workedCurrentJob = false;
+        }
+
+    };
+
+    //add/update button
+    $scope.updateEmpDetails = function (id, data) {
+        $("#addEmpDetailsPopup").modal({show: false, backdrop: "static", keyboard: false});
+        $rootScope.showLoader = true;
+        if(id == undefined || id == ""){
+            id = "";
+            data.id = id;
+        }
+        console.log("data - ");
+        console.log(data);
+        var addOrUpdateEmpDetailsParams = { "user": data, "tableName": "empHistory", "type": "addOrUpdateEmpHistory" };
+        var cat = postData.crud(addOrUpdateEmpDetailsParams);
+        cat.then(function (response) {
+            if(response == "error"){
+                postData.setResult("error", postData.alertMessages("error"));
+            }else if(response == "success"){
+                postData.setResult("success", postData.alertMessages("success"));
+            }else{
+
+            }
+            $scope.getEmpDetails();
+            $scope.isDisable = true;
+            $scope.showAddEmpDetails = false;
+            $rootScope.cancelBtn = false;
+            $rootScope.addBtn = true;
+            $rootScope.showLoader = false;    
+        });
+    };
+
+    $scope.deleteIndEmpDetails = function(id,index){
+        var result = confirm("Are you sure you want to delete?");
+        if (result) {
+            var data = {
+                "id" : id,
+                "index" : index
+            };
+            var deleteEmpDetailsDataParams = { "user": data, "tableName": "empHistory", "type": "deleteEmpHistory" };
+            var cat = postData.crud(deleteEmpDetailsDataParams);
+            cat.then(function (response) {
+                if(response == "deleted"){
+                    postData.setResult("deleted", postData.alertMessages("deleted"));
+                }else if(response == "error"){
+                    postData.setResult("error", postData.alertMessages("error"));
+                }else{
+
+                }
+            });
+            
+            $rootScope.showLoader = true;
+            $scope.showAddEmpDetails = false;
+            $rootScope.cancelBtn = false;
+            $rootScope.addBtn = true;
+            $scope.empSelectedDetailsData.splice(index, 1);
+            $rootScope.showLoader = false;
+        }
+    };
+
+    $scope.closeEmpDetails = function(){
+        $("#addEmpDetailsPopup").modal({show: false, backdrop: "static", keyboard: false});
+        $rootScope.showLoader = true;
+        $scope.isDisable = true;
+        $scope.showAddEmpDetails = false;
+        $rootScope.cancelBtn = false;
+        $rootScope.addBtn = true;
+        $rootScope.showLoader = false;
+    };
+
+    $scope.cancelEmpDetails = function () {
+        $rootScope.showLoader = true;
+        $scope.isDisable = true;
+        $scope.showAddEmpDetails = false;
+        $rootScope.cancelBtn = false;
+        $rootScope.addBtn = true;
+        $rootScope.showLoader = false;
+    };
+
+
+    //ini getdetails
+    $scope.getEmpDetails();
+    //ctrls ends
+
+    function formatDateToString(date){
+        // 01, 02, 03, ... 29, 30, 31
+        var dd = (date.getDate() < 10 ? '0' : '') + date.getDate();
+        // 01, 02, 03, ... 10, 11, 12
+        var MM = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
+        // 1970, 1971, ... 2015, 2016, ...
+        var yyyy = date.getFullYear(); 
+
+        return (yyyy + "-" + MM + "-" + dd);
+     }
+
+});
+ 
+
+    // [
+    //     {
+    //       "id": "46",
+    //       "email": "user1@gmail.com",
+    //       "empHistory": "2012 - System  Analyst - Xandato LtdAnalysed system architecture, performed data mining analysis on thousands of entries of raw data and monitored and maintaned the ststem analysis software.",
+    //       "workedCompany": "cnamee",
+    //       "workedYear": "",
+    //       "workedMonth": "",
+    //       "workedJoinDate": "2015-03-25T00:00:00.000Z",
+    //       "workedEndDate": "2015-03-25T00:00:00.000Z",
+    //       "workedCurrentJob": "true"
+    //     },
+    //     {
+    //       "id": "48",
+    //       "email": "user1@gmail.com",
+    //       "empHistory": "2010 - IT Administrator - Tosox Corp Oversaw the Companys IT infrastrucre, managed and maintaned the IT and multimedia network.",
+    //       "workedCompany": "cname22",
+    //       "workedYear": "",
+    //       "workedMonth": "",
+    //       "workedJoinDate": "2015-03-25",
+    //       "workedEndDate": "2015-03-25",
+    //       "workedCurrentJob": "false"
+    //     }
+    //   ]
