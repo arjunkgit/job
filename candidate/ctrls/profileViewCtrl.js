674f1bd71e1b11d44ffce4fe6e1c0396d4703644
ArjunKulkarni <arjunkneworld@gmail.com>
2018-09-25 17:50:39 +0530

    Create profileViewCtrl.js

---
+++ candidate/ctrls/profileViewCtrl.js
@@ -0,0 +1,16 @@
+candidateApp.controller("profileViewCtrl", function($scope, $filter,$location, $http,NgTableParams) {
+
+//initial Buttons
+$scope.profileEdit_add = false;
+$scope.profileEdit_edit = true;
+$scope.profileEdit_save = false;
+$scope.profileEdit_cancel = false;
+    
+$scope.dataa = "Pro View";
+
+$scope.editProfile = function(){
+    $location.path("profileEdit");
+};
+
+//ctrls ends
+});
