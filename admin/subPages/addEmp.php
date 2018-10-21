bf554b1dc65d10d25a0f43ddb4298183920c361f
ArjunKulkarni <arjunkneworld@gmail.com>
2018-10-21 21:12:55 +0530

    Create addEmp.php

---
+++ admin/subPages/addEmp.php
@@ -0,0 +1,41 @@
+<style>
+#ResultData{
+    display: none;
+    position: fixed;
+    z-index: 11;
+    right: 0;
+    top: 0;
+}
+
+.jb-grid-tile {
+	border:1px solid #d4d4d4;
+	background-color: white;
+}
+.dialog-background{
+    background: none repeat scroll 0 0 rgba(155, 155, 155, 0.5);
+    height: 100%;
+    left: 0;
+    margin: 0;
+    padding: 0;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 100;
+}
+.dialog-loading-wrapper {
+    background: none repeat scroll 0 0 rgba(0, 0, 0, 0);
+    border: 0 none;
+    height: 100px;
+    left: 50%;
+    margin-left: -50px;
+    margin-top: -50px;
+    position: fixed;
+    top: 50%;
+    width: 100px;
+    z-index: 9999999;
+}
+.dialog-loading-icon {
+    background-color: #FFFFFF !important;
+    border-radius: 13px;
+    display: block;
+    height: 100px;
