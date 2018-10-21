8a4b7f1c6eedf52ee1e7bbe1231c23147df2c776
ArjunKulkarni <arjunkneworld@gmail.com>
2018-10-21 21:13:58 +0530

    Create dashboard.php

---
+++ admin/subPages/dashboard.php
@@ -0,0 +1,41 @@
+<div ng-controller="dashboard">
+<style>
+.jb-grid-tile {
+	border:1px solid #d4d4d4;
+	background-color: white;
+}
+</style>
+<div class="row">
+	<div class="col-md-6">
+		<div class="panel panel-default">
+		  <div class="panel-heading">Statistcs</div>
+		  <div class="panel-body">
+			 <ul class="list-group">
+<?php 
+		require_once ("../../db.php");
+		//Queries
+		$totalJobs = "SELECT * FROM `jobsPost`";
+		$totalEmp = "SELECT * FROM `employerRegData`";
+		$totalSeek = "SELECT * FROM `candidateRegData`";
+		
+		//execute
+		$totalJobsResult = mysqli_query($con, $totalJobs);		
+		$totalEmpResult = mysqli_query($con, $totalEmp);		
+		$totalSeeksResult = mysqli_query($con, $totalSeek);		
+		
+		
+		//Show result
+		echo '<li class="list-group-item">Number of Jobs Posted <span class="badge">'.mysqli_num_rows($totalJobsResult).'</span></li>';
+		echo '<li class="list-group-item">Number of Employer <span class="badge">'.mysqli_num_rows($totalEmpResult).'</span></li>';
+		echo '<li class="list-group-item">Number of  Job Seekers <span class="badge">'.mysqli_num_rows($totalSeeksResult).'</span></li>';
+		mysqli_close($con);
+?>			 
+			</ul> 
+		  </div>
+		</div>	
+	</div>	
+	<div class="col-md-6">
+		<div class="panel panel-default">
+		  <div class="panel-heading">Panel Heading</div>
+		  <div class="panel-body">Panel Content</div>
+		</div>	
