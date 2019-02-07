a1cad5c451a2e5c9cd3fbb39398eb4020143eaa1
ArjunKulkarni <arjunkneworld@gmail.com>
2019-02-07 21:18:12 +0530

    Update currentOpenSlideUp.php

--- currentOpenSlideUp.php
+++ currentOpenSlideUp.php
@@ -1,36 +1,63 @@
+<style>
+  .panel-default {
+    border-color: unset;
+    border: 0px;
+    border: 1px solid #e2e2e2;
+}
+.panel-default>.panel-heading {
+    color: #fff;
+    background-color: #0e5b62;
+    border-color: #ddd;
+    font-size: 14px;
+    font-weight: bold;
+}
+
+.panel-body {
+    margin-bottom: 10px;
+    font-size: 13px;
+    color: #545454;
+}
+.panel-default {
+			border-color: unset;
+			border: 1px solid #e0e0e0;
+			padding: 0px;
+			margin: 5px;
+		}
+
+</style>
 <div class="currentUp">
-<div class="headings" ><b>Current Openings</b></h5></div>
+<div class="headings" >Current Openings</h5></div>
  <marquee height="450px"  scrollamount="5" direction = "up">
 <div class="panel panel-default">
   <div class="panel-heading">Software Engineer</div>
   <div class="panel-body">Location : Bangalore</div>
 </div>
-<br>
+
 <div class="panel panel-default">
   <div class="panel-heading">Support Engineer</div>
   <div class="panel-body">Location : Mysore</div>
 </div>
-<br>
+
 <div class="panel panel-default">
   <div class="panel-heading">Test Engineer</div>
   <div class="panel-body">Location : Chennai</div>
 </div>
-<br>
+
 <div class="panel panel-default">
   <div class="panel-heading">Test Engineer</div>
   <div class="panel-body">Location : Chennai</div>
 </div>
-<br>
+
 <div class="panel panel-default">
   <div class="panel-heading">Design Engineer</div>
   <div class="panel-body">Location : Bangalore</div>
 </div>
-<br>
+
 <div class="panel panel-default">
   <div class="panel-heading">Test Engineer</div>
   <div class="panel-body">Location : Chennai</div>
 </div>
-<br>
+
 <div class="panel panel-default">
   <div class="panel-heading">Test Engineer</div>
   <div class="panel-body">Location : Chennai</div>
