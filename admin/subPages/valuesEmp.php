b03e0ce5293fb770bdfb13ad7c21af0d1a2cea9e
ArjunKulkarni <arjunkneworld@gmail.com>
2019-02-07 20:51:03 +0530

    Update valuesEmp.php

--- admin/subPages/valuesEmp.php
+++ admin/subPages/valuesEmp.php
@@ -1,3 +1,35 @@
+<style>
+#ResultData{
+    display: none;
+    position: fixed;
+    z-index: 11;
+    right: 0;
+    top: 0;
+}
+
+.jb-grid-tile {
+	border:1px solid #d4d4d4;
+	background-color: white;
+}
+.dialog-background{
+    background: none repeat scroll 0 0 rgba(155, 155, 155, 0.5);
+    height: 100%;
+    left: 0;
+    margin: 0;
+    padding: 0;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 100;
+}
+.dialog-loading-wrapper {
+    background: none repeat scroll 0 0 rgba(0, 0, 0, 0);
+    border: 0 none;
+    height: 100px;
+    left: 50%;
+    margin-left: -50px;
+    margin-top: -50px;
+    position: fixed;
     top: 50%;
     width: 100px;
     z-index: 9999999;
@@ -38,3 +70,27 @@
         <tbody>
           <tr ng-repeat="row in $data">
                 <td data-title="'Email Id'" filter="{email: 'text'}" 
+                	filter-data="email" sortable="'email'">
+                	{{ row.email }}</td>
+                <td data-title="'Create New Employer Limit'" filter="{createNewEmpLimit: 'text'}" 
+                	filter-data="createNewEmpLimit" sortable="'createNewEmpLimit'">
+                	{{ row.createNewEmpLimit }}</td>
+                <td data-title="'Jobs Post Limit'" filter="{jobsPostLimit: 'text'}" 
+                	filter-data="jobsPostLimit" sortable="'jobsPostLimit'">
+                	{{ row.jobsPostLimit }}</td>
+				<td data-title="'Action'" > 
+					<button class="btn btn-info btn-sm" data-ng-click="editEmpLimit(row)" style="cursor: pointer;">
+						<span class="glyphicon glyphicon-edit"></span> 
+					</button>
+				</td>    	        	
+		  </tr>
+        </tbody>
+		</table>	  
+	</div>
+</div>
+
+
+	
+</div>
+
+</div>
