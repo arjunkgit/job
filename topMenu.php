99afe4368bc0eef1bec676253532738581fc77e1
ArjunKulkarni <arjunkneworld@gmail.com>
2019-02-07 21:26:52 +0530

    Update topMenu.php

--- topMenu.php
+++ topMenu.php
@@ -1,4 +1,4 @@
-﻿<div class="navbar-header">
+<div class="navbar-header">
     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
@@ -6,33 +6,36 @@
         <span class="icon-bar"></span>
     </button>
     <a class="navbar-brand" href="index.php">
-    <span style="color: white;font-size: 40px;position: absolute;">
-		VoqeoIT
-	</span> 
+		<span style="color: black; font-size: 1.6rem;line-height: 100%;font-weight: 500;text-decoration: none;text-transform: uppercase;font-family: sans-serif;">MNG</span>
+		<span style="font-weight: 300;font-size: 1.6rem;line-height: 100%;" class="span12">GBS</span>
 	<!--
-	    <img src="images/log.png" width="220" height="35" />
+	    <img src="images/log.png" width="220" height="35" /> 
 	-->
     </a>
 </div>
+
 <!--/.navbar-header-->
-<div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1" style="height: 1px;">
+<div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1" style="height: 1px;    margin: -30px;">
     <ul class="nav navbar-nav">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="aboutus.php">About Us</a></li>
+        <li><a href="http://www.voqeoit.com">Home</a></li>
+		<li><a href="http://www.voqeoit.com/#services">Services</a></li>
+		
 		<?php
 		if (isset($_SESSION["username"]))
 		{
-			echo "<li><a href='currentOpenings.php'>Current Openings</a></li>";			
+			echo "<li><a href='currentOpenings.php'>Current Opening</a></li>";			
 		}
 		else
 		if (isset($_SESSION["empusername"])){
 						
 		}
 		else{
-			echo "<li><a href='currentOpenings.php'>Current Openings</a></li>";			
+			echo "<li><a href='http://www.voqeoit.com/#current-opening'>Current Opening</a></li>";			
 		}
 		?>        
-        <li><a href="contactus.php">Contact Us</a></li>
+        <li><a href="http://www.voqeoit.com/#clients-1">Client</a></li>
+        <li><a href="http://www.voqeoit.com/#contact-1">Contact</a></li>
+        <li><a href="http://www.voqeoit.com/#testimonials-5">Testimonials</a></li>
     </ul>
 </div>
 <div class="clearfix"> </div>
