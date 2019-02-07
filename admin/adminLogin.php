486437beedf15489a1a22d0daa07db2bde8432c2
ArjunKulkarni <arjunkneworld@gmail.com>
2019-02-07 20:51:24 +0530

    Update adminLogin.php

--- admin/adminLogin.php
+++ admin/adminLogin.php
@@ -1,4 +1,4 @@
-oooo<?php
+<?php
 	session_start();
 ?>
 <!DOCTYPE HTML>
@@ -39,3 +39,104 @@ md-card md-card-title md-card-title-media .md-media-xs {
 	    <md-toolbar class="md-hue-2">
 	      <div class="md-toolbar-tools">
 			<h2 flex md-truncate>Admin Login</h2>
+	      </div>
+	    </md-toolbar>
+		<div layout="column" layout-padding>
+<?php
+	require('../db.php');
+    // If form submitted, insert values into the database.
+    if (isset($_REQUEST['adminLoginSubmit'])){
+		$email = $_POST['email'];
+		$password = stripslashes($_REQUEST['password']);
+		$password = mysqli_real_escape_string($con,$password);
+	//Checking is user existing in the database or not
+        $query = "SELECT * FROM `adminregdata` WHERE email='$email' and password='".md5($password)."' "; 
+
+	
+      $result = mysqli_query($con,$query);
+		if ( false===$result ) {
+		printf("error: %s\n", mysqli_error($con));
+		}
+		else {
+		// echo 'done2.';
+		}				
+		$rows = mysqli_num_rows($result);
+        if($rows==1){
+			$_SESSION['adminusername'] = $email;
+			header("Location: index.php"); // Redirect user to index.php
+            }else{
+	echo "<div class='form' style='text-align: center;'><h3>Username/password is incorrect.</h3></div>";
+				}
+    }else{
+
+}?>
+
+<?php
+if (!isset($_SESSION["adminusername"]))
+						{
+?>
+
+
+
+<div layout="column" layout-align="center center">
+
+  <div flex="100">
+
+          <form action="adminLogin.php" method="post" name="login" style="padding: 10px;border: 1px solid #e7e7e7;background-color: white;">
+            <div class="form-group">
+              <label for="usrname"><span class="glyphicon glyphicon-user"></span> Email ID</label>
+              <input type="text" class="form-control" id="usrname" name="email" placeholder="Enter email" required>
+            </div>
+            <div class="form-group">
+              <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+              <input type="password" class="form-control" id="psw"  name="password" placeholder="Enter password" required>
+            </div>
+              <button type="submit" class="btn btn-success btn-block" name="adminLoginSubmit">
+              <span class="glyphicon glyphicon-off"></span> Login</button>
+          </form> 
+  </div>
+</div>
+
+
+
+
+<?php
+						}
+						else
+						{
+?>
+
+<div> 
+	Logged in Admin
+</div>
+
+
+<?php							
+							
+						}
+?>
+
+
+		</div>
+      <div flex></div>
+    </md-content>
+  </section>
+</div>
+  
+  
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-route.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/angularjs//1.6.4/angular-animate.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-aria.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-messages.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/ng-table/0.8.3/ng-table.min.js"></script> 
+<script>
+var app2 = angular.module('adminModule', ['ngMaterial']);	
+	app2.controller('loginCtrl', function($scope){
+		$scope.Name = "name1";
+	});
+</script>
+
+</body>
+</html> 
