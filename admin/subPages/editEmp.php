4855e53297a5a9a37096a0ba42fa03c0db443c7f
ArjunKulkarni <arjunkneworld@gmail.com>
2019-02-07 20:50:21 +0530

    Update editEmp.php

--- admin/subPages/editEmp.php
+++ admin/subPages/editEmp.php
@@ -39,3 +39,28 @@
     border-radius: 13px;
     display: block;
     height: 100px;
+    line-height: 100px;
+    margin: 0;
+    padding: 1px;
+    text-align: center;
+    width: 100px;
+}
+
+</style>
+
+<div ng-controller="manageEmp">
+<div id="ResultData"  class="alert  alert-dismissable">
+	<span class=""></span> 
+	<a href="" class="close" data-dismiss="alert" aria-label="close">*</a>
+	<span id="ResultMessage"></span>
+</div>
+<div class="dialog-background" ng-show="showLoader">
+    <div class="dialog-loading-wrapper">
+        <md-progress-circular class="dialog-loading-icon"  md-diameter="55px"></md-progress-circular>
+	</div>
+</div>
+
+
+
+</div>
+
