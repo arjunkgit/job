9fa09931d81ffa2465f708704f4e2c7162906d66
ArjunKulkarni <arjunkneworld@gmail.com>
2019-02-07 20:55:45 +0530

    Update profileViewCtrl.js

--- candidate/ctrls/profileViewCtrl.js
+++ candidate/ctrls/profileViewCtrl.js
@@ -1,16 +1,13 @@
-candidateApp.controller("profileViewCtrl", function($scope, $filter,$location, $http,NgTableParams) {
+candidateApp.controller("profileViewCtrl", function ($scope, $filter, $location, $http, NgTableParams) {
+    //initial Buttons
+    $scope.profileEdit_add = false;
+    $scope.profileEdit_edit = true;
+    $scope.profileEdit_save = false;
+    $scope.profileEdit_cancel = false;
+    $scope.dataa = "Pro View";
+    $scope.editProfile = function () {
+        $location.path("profileEdit");
+    };
 
-//initial Buttons
-$scope.profileEdit_add = false;
-$scope.profileEdit_edit = true;
-$scope.profileEdit_save = false;
-$scope.profileEdit_cancel = false;
-    
-$scope.dataa = "Pro View";
-
-$scope.editProfile = function(){
-    $location.path("profileEdit");
-};
-
-//ctrls ends
+    //ctrls ends
 });
