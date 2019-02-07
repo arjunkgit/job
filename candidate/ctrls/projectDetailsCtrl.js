e8f5d348a3ebb1bac387158eb1304b8e20b51d7c
ArjunKulkarni <arjunkneworld@gmail.com>
2019-02-07 20:56:23 +0530

    Update projectDetailsCtrl.js

--- candidate/ctrls/projectDetailsCtrl.js
+++ candidate/ctrls/projectDetailsCtrl.js
@@ -1,5 +1,4 @@
 candidateApp.controller("projectDetailsCtrl", function ($scope, $rootScope, postData, $filter, $http, NgTableParams) {
-
     var projectDetailsData = {};
     var stringWorkedJoinDate = "";
     var stringWorkedEndDate = "";
@@ -13,7 +12,7 @@ candidateApp.controller("projectDetailsCtrl", function ($scope, $rootScope, post
     // $scope.empSelectedDetailsData = [{
     //     "id": "48",
     //     "email": "user1@gmail.com",
-    //     "empHistory": "2010 - IT Administrator - Tosox Corp Oversaw the Companys IT infrastrucre, managed and maintaned the IT and multimedia network.",
+    //     "emphistory": "2010 - IT Administrator - Tosox Corp Oversaw the Companys IT infrastrucre, managed and maintaned the IT and multimedia network.",
     //     "workedCompany": "cname22",
     //     "workedYear": "",
     //     "workedMonth": "",
@@ -24,7 +23,7 @@ candidateApp.controller("projectDetailsCtrl", function ($scope, $rootScope, post
     // {
     //     "id": "49",
     //     "email": "user1@gmail.com",
-    //     "empHistory": "2010 - IT Administrator - Tosox Corp Oversaw the Companys IT infrastrucre, managed and maintaned the IT and multimedia network.",
+    //     "emphistory": "2010 - IT Administrator - Tosox Corp Oversaw the Companys IT infrastrucre, managed and maintaned the IT and multimedia network.",
     //     "workedCompany": "cname22",
     //     "workedYear": "",
     //     "workedMonth": "",
@@ -35,7 +34,7 @@ candidateApp.controller("projectDetailsCtrl", function ($scope, $rootScope, post
 
     //get emp details
     $scope.getEmpDetails = function () {
-        var getEmpDetailsData = { "tableName": "projectHistory", "type": "getUserData" };
+        var getEmpDetailsData = { "tableName": "projecthistory", "type": "getUserData" };
         var cat2 = postData.getUserData(getEmpDetailsData);
         cat2.then(function (response) {
             $scope.empSelectedDetailsData = response.data;
@@ -45,12 +44,12 @@ candidateApp.controller("projectDetailsCtrl", function ($scope, $rootScope, post
     };
 
     $scope.addEmpDetails = function () {
-        $("#addProjectDetailsPopup").modal({show: true, backdrop: "static", keyboard: false});
+        $("#addProjectDetailsPopup").modal({ show: true, backdrop: "static", keyboard: false });
         $scope.empShowDetailsData = {
-            projectTitle : "",
-            client : "",
-            duration : "",
-            projectDetails : ""
+            projectTitle: "",
+            client: "",
+            duration: "",
+            projectDetails: ""
         };
         $rootScope.showLoader = true;
         $scope.isDisable = false;
@@ -60,15 +59,15 @@ candidateApp.controller("projectDetailsCtrl", function ($scope, $rootScope, post
         $rootScope.showLoader = false;
     };
 
-  $scope.convertToDate = function (stringDate){
-    var dateOut = new Date(stringDate);
-    dateOut.setDate(dateOut.getDate());
-    return dateOut;
-  };
+    $scope.convertToDate = function (stringDate) {
+        var dateOut = new Date(stringDate);
+        dateOut.setDate(dateOut.getDate());
+        return dateOut;
+    };
     //onclick of update
-    $scope.updateIndEmpDetails = function (id, data){
-        $("#addProjectDetailsPopup").modal({show: true, backdrop: "static", keyboard: false});
-        $scope.empShowDetailsData = data;     
+    $scope.updateIndEmpDetails = function (id, data) {
+        $("#addProjectDetailsPopup").modal({ show: true, backdrop: "static", keyboard: false });
+        $scope.empShowDetailsData = data;
         $rootScope.showLoader = true;
         $scope.isDisable = false;
         $scope.showAddEmpDetails = true;
@@ -79,21 +78,21 @@ candidateApp.controller("projectDetailsCtrl", function ($scope, $rootScope, post
 
     //add/update button
     $scope.updateEmpDetails = function (id, data) {
-        $("#addProjectDetailsPopup").modal({show: false, backdrop: "static", keyboard: false});
+        $("#addProjectDetailsPopup").modal({ show: false, backdrop: "static", keyboard: false });
         $rootScope.showLoader = true;
-        if(id == undefined || id == ""){
+        if (id == undefined || id == "") {
             id = "";
             data.id = id;
         }
-        var addOrUpdateEmpDetailsParams = { "user": data, "tableName": "projectHistory", "type": "addOrUpdateProjectHistory" };
+        var addOrUpdateEmpDetailsParams = { "user": data, "tableName": "projecthistory", "type": "addOrUpdateProjectHistory" };
         var cat = postData.crud(addOrUpdateEmpDetailsParams);
         cat.then(function (response) {
             console.log(response);
-            if(response == "error"){
+            if (response == "error") {
                 postData.setResult("error", postData.alertMessages("error"));
-            }else if(response == "success"){
+            } else if (response == "success") {
                 postData.setResult("success", postData.alertMessages("success"));
-            }else{
+            } else {
 
             }
             $scope.getEmpDetails();
@@ -101,30 +100,29 @@ candidateApp.controller("projectDetailsCtrl", function ($scope, $rootScope, post
             $scope.showAddEmpDetails = false;
             $rootScope.cancelBtn = false;
             $rootScope.addBtn = true;
-            $rootScope.showLoader = false;    
+            $rootScope.showLoader = false;
         });
     };
 
-    $scope.deleteIndEmpDetails = function(id,index){
+    $scope.deleteIndEmpDetails = function (id, index) {
         var result = confirm("Are you sure you want to delete?");
         if (result) {
             //Logic to delete the item
             var data = {
-                "id" : id,
-                "index" : index
+                "id": id,
+                "index": index
             }
-            var deleteEmpDetailsDataParams = { "user": data, "tableName": "projectHistory", "type": "deleteEmpHistory" };
+            var deleteEmpDetailsDataParams = { "user": data, "tableName": "projecthistory", "type": "deleteEmpHistory" };
             var cat = postData.crud(deleteEmpDetailsDataParams);
             cat.then(function (response) {
-                if(response == "deleted"){
+                if (response == "deleted") {
                     postData.setResult("deleted", postData.alertMessages("deleted"));
-                }else if(response == "error"){
+                } else if (response == "error") {
                     postData.setResult("error", postData.alertMessages("error"));
-                }else{
+                } else {
 
                 }
             });
-            
             $rootScope.showLoader = true;
             $scope.showAddEmpDetails = false;
             $rootScope.cancelBtn = false;
@@ -134,8 +132,8 @@ candidateApp.controller("projectDetailsCtrl", function ($scope, $rootScope, post
         }
     };
 
-    $scope.closeEmpDetails = function(){
-        $("#addProjectDetailsPopup").modal({show: false, backdrop: "static", keyboard: false});
+    $scope.closeEmpDetails = function () {
+        $("#addProjectDetailsPopup").modal({ show: false, backdrop: "static", keyboard: false });
         $rootScope.showLoader = true;
         $scope.isDisable = true;
         $scope.showAddEmpDetails = false;
@@ -158,25 +156,25 @@ candidateApp.controller("projectDetailsCtrl", function ($scope, $rootScope, post
     $scope.getEmpDetails();
     //ctrls ends
 
-    function formatDateToString(date){
+    function formatDateToString(date) {
         // 01, 02, 03, ... 29, 30, 31
         var dd = (date.getDate() < 10 ? '0' : '') + date.getDate();
         // 01, 02, 03, ... 10, 11, 12
         var MM = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
         // 1970, 1971, ... 2015, 2016, ...
-        var yyyy = date.getFullYear(); 
+        var yyyy = date.getFullYear();
 
         return (yyyy + "-" + MM + "-" + dd);
-     }
+    }
 
 });
- 
+
 
     // [
     //     {
     //       "id": "46",
     //       "email": "user1@gmail.com",
-    //       "empHistory": "2012 - System  Analyst - Xandato LtdAnalysed system architecture, performed data mining analysis on thousands of entries of raw data and monitored and maintaned the ststem analysis software.",
+    //       "emphistory": "2012 - System  Analyst - Xandato LtdAnalysed system architecture, performed data mining analysis on thousands of entries of raw data and monitored and maintaned the ststem analysis software.",
     //       "workedCompany": "cnamee",
     //       "workedYear": "",
     //       "workedMonth": "",
@@ -187,7 +185,7 @@ candidateApp.controller("projectDetailsCtrl", function ($scope, $rootScope, post
     //     {
     //       "id": "48",
     //       "email": "user1@gmail.com",
-    //       "empHistory": "2010 - IT Administrator - Tosox Corp Oversaw the Companys IT infrastrucre, managed and maintaned the IT and multimedia network.",
+    //       "emphistory": "2010 - IT Administrator - Tosox Corp Oversaw the Companys IT infrastrucre, managed and maintaned the IT and multimedia network.",
     //       "workedCompany": "cname22",
     //       "workedYear": "",
     //       "workedMonth": "",
