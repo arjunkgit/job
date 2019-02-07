bfb9f5cc5ce34364ae2eef2df6166e020a126cc6
ArjunKulkarni <arjunkneworld@gmail.com>
2019-02-07 20:52:26 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -39,3 +39,133 @@ md-card md-card-title md-card-title-media .md-media-xs {
 }
 
 md-content.md-default-theme, md-content{
+	background-color: rgb(255, 255, 255) !important;
+}
+
+
+</style>
+</head>
+<body ng-app="adminModule" ng-controller="mainCtrl">
+  <section layout="row" flex>
+
+    <md-sidenav  layout="column" style="height:100vh;"
+        class="md-sidenav-left flex"
+        md-component-id="left"
+        md-is-locked-open="$mdMedia('gt-md')"
+        md-whiteframe="4">
+
+      <md-toolbar class="md-theme-indigo" style="flex-direction: row;">
+		<span style="flex-grow: 7"><h1 class="md-toolbar-tools">List of options</h1></span>
+		<span ng-click="closeLeft()" hide-gt-md style="flex-grow: 3"><h1 class="md-toolbar-tools">close</h1></span>
+      </md-toolbar>
+
+	    <div flex-xs flex-gt-xs="100" layout="column">
+	      <md-card md-theme="default" md-theme-watch>
+			<md-button href="#!/"  ng-click="closeLeft()">
+			        <md-card-title class="padding-top-bottom-5px">
+			          <md-card-title-media style="margin-top: 0px !important;">
+			            <div class="md-media-xs card-media" style="border-radius: 50px;">
+						 <span class="glyphicon glyphicon-equalizer md-headline"></span> 			            	
+			            </div>
+			          </md-card-title-media>
+			          <md-card-title-text>
+			            <span class="md-title noCap">Dashboard</span>
+			          </md-card-title-text>
+			        </md-card-title>	
+			</md-button>
+			<md-button href="#!addEmp" ng-click="closeLeft()">
+			        <md-card-title class="padding-top-bottom-5px">
+			          <md-card-title-media style="margin-top: 0px !important;">
+			            <div class="md-media-xs card-media" style="border-radius: 50px;">
+						 <span class="glyphicon glyphicon-plus md-headline"></span> 			            	
+			            </div>
+			          </md-card-title-media>
+			          <md-card-title-text>
+			            <span class="md-title noCap">Add New Employer</span>
+			          </md-card-title-text>
+			        </md-card-title>	
+			</md-button>
+			<md-button href="#!manageEmp"  ng-click="closeLeft()">
+			        <md-card-title class="padding-top-bottom-5px">
+			          <md-card-title-media style="margin-top: 0px !important;">
+			            <div class="md-media-xs card-media" style="border-radius: 50px;">
+						 <span class="glyphicon glyphicon-file md-headline"></span> 			            	
+			            </div>
+			          </md-card-title-media>
+			          <md-card-title-text>
+			            <span class="md-title noCap">Manage Employer</span>
+			          </md-card-title-text>
+			        </md-card-title>	
+			</md-button>
+			<md-button  href="#!listCandidate"  ng-click="closeLeft()">
+			        <md-card-title class="padding-top-bottom-5px">
+			          <md-card-title-media style="margin-top: 0px !important;">
+			            <div class="md-media-xs card-media" style="border-radius: 50px;">
+						 <span class="glyphicon glyphicon-list-alt md-headline"></span> 			            	
+			            </div>
+			          </md-card-title-media>
+			          <md-card-title-text>
+			            <span class="md-title noCap">JobSeekers List</span>
+			          </md-card-title-text>
+			        </md-card-title>	
+			</md-button>
+			<md-button  href="#!valuesEmp"  ng-click="closeLeft()">
+			        <md-card-title class="padding-top-bottom-5px">
+			          <md-card-title-media style="margin-top: 0px !important;">
+			            <div class="md-media-xs card-media" style="border-radius: 50px;">
+						 <span class="glyphicon glyphicon-list-alt md-headline"></span> 			            	
+			            </div>
+			          </md-card-title-media>
+			          <md-card-title-text>
+			            <span class="md-title noCap">Employer values</span>
+			          </md-card-title-text>
+			        </md-card-title>	
+			</md-button>
+			<md-button  href="#!listJobs"  ng-click="closeLeft()">
+			        <md-card-title class="padding-top-bottom-5px">
+			          <md-card-title-media style="margin-top: 0px !important;">
+			            <div class="md-media-xs card-media" style="border-radius: 50px;">
+						 <span class="glyphicon glyphicon-tasks md-headline"></span> 			            	
+			            </div>
+			          </md-card-title-media>
+			          <md-card-title-text>
+			            <span class="md-title noCap">View/Delete Posted Jobs</span>
+			          </md-card-title-text>
+			        </md-card-title>	
+			</md-button>
+	      </md-card>
+		</div>
+    </md-sidenav>
+
+    <md-content flex>
+	    <md-toolbar class="md-hue-2">
+	      <div class="md-toolbar-tools">
+			<md-button ng-click="toggleLeft()" aria-label="Settings" ng-disabled="false"
+				class="md-icon-button" show-md hide-gt-md>
+				<md-icon class="glyphicon glyphicon-menu-hamburger" style="margin-top: 5px;"></md-icon>
+			</md-button>
+			<h2 flex md-truncate>Admin Panel</h2>
+			<md-button href="logout.php" class="md-icon-button" aria-label="More"> 
+			  <md-icon class="glyphicon glyphicon-log-out"></md-icon>
+			</md-button>
+	      </div>
+	    </md-toolbar>
+		<div layout="column" layout-padding>
+			<div ng-view></div>
+		</div>
+      <div flex></div>
+    </md-content>
+  </section>
+</div>
+<?php include('footer.php'); ?>
+  
+  
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-route.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/angularjs//1.6.4/angular-animate.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-aria.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-messages.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/ng-table/0.8.3/ng-table.min.js"></script> 
+</body>
+</html> 
