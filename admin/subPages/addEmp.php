788b92f149e1b70499d8ea7f75565d403d971bfd
ArjunKulkarni <arjunkneworld@gmail.com>
2019-02-07 20:48:18 +0530

    Update addEmp.php

--- admin/subPages/addEmp.php
+++ admin/subPages/addEmp.php
@@ -39,3 +39,57 @@
     border-radius: 13px;
     display: block;
     height: 100px;
+    line-height: 100px;
+    margin: 0;
+    padding: 1px;
+    text-align: center;
+    width: 100px;
+}
+
+</style>
+
+<div ng-controller="addEmp" data-ng-init="init()">
+<div id="ResultData"  class="alert  alert-dismissable">
+	<span class=""></span> 
+	<a href="" class="close" data-dismiss="alert" aria-label="close">*</a>
+	<span id="ResultMessage"></span>
+</div>
+<div class="dialog-background" ng-show="showLoader">
+    <div class="dialog-loading-wrapper">
+        <md-progress-circular class="dialog-loading-icon"  md-diameter="55px"></md-progress-circular>
+	</div>
+</div>
+
+<div class="col-md-12">
+<div class="panel panel-default">
+	  <md-subheader class="md-no-sticky">Add New Employer</md-subheader>
+		 <md-content layout-padding>
+		    <div>
+		      <form name="userForm">
+		        <md-input-container class="md-block">
+		          <label>Email</label>
+		          <input type="email" ng-required="true" placeholder="Email (required)" ng-model="empReg.email">
+		        </md-input-container>
+		        <md-input-container class="md-block">
+		          <label>Password</label>
+		          <input type="password" ng-required="true" placeholder="Password (required)" ng-model="empReg.password">
+		        </md-input-container>
+		        <md-input-container class="md-block">
+		          <label>Company Name</label>
+		          <input  ng-model="empReg.companyname">
+		        </md-input-container>
+		        <md-input-container class="md-block">
+		          <label>Mobile</label>
+		          <input  ng-model="empReg.mobile">
+		        </md-input-container>
+				<md-button ng-disabled="!empReg.email" ng-click="addEmp()" class="md-primary">
+					Add Employer
+				</md-button>
+		      </form>
+		    </div>
+		 </md-content>
+</div>	
+</div>
+
+</div>
+
